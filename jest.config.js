496a8530fa81411e3c02b357607017906ef2c78e
Guillermo Quiros <gquiman@gmail.com>
2018-09-06 14:40:01 +1200

    Config Jets

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,10 @@ module.exports = {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
         "\\.(css|less)$": "identity-obj-proxy"
     },
+    testPathIgnorePatterns:["/node_modules/","<rootDir>/scr/test/TestSetup.js"],
+    modulePathIgnorePatterns:["./scr/test/TestSetup.js"],
+    coverageDirectory: "./coverage/",
     collectCoverage:true,
     coverageReporters: ["lcov", "text"],
-    setupFiles:["./scr/test/TestSetup.js"]//,
-    //reporters=["jest-junit"]
+    setupFiles:["./src/test/TestSetup.js"]
 };
