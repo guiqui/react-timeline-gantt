4103e3d6f201ddfb4e8b45f7a34d1cb2a873682c
Guillermo Quiros <gquiman@gmail.com>
2018-10-28 22:35:23 +1300

    Improving Unit Test

--- src/lib/components/header/Headers.spec.js
+++ src/lib/components/header/Headers.spec.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Headers from './Headers';
 import DateHelper from 'libs/helpers/DateHelper'
+import moment from  'moment'
 import { shallow ,mount} from 'enzyme';
 
 
-describe('VirtualListCore Initialise propertly ',()=>{
+describe('Header Initi ',()=>{
     it ('It mount properly when no property is given',()=>{
         const wrapper = shallow(<Headers />);
         expect(wrapper.find('#timeline-header')).toBeDefined();
@@ -13,20 +14,16 @@ describe('VirtualListCore Initialise propertly ',()=>{
 
     it ('It properties are assign properly and state',()=>{
         //calculateMonthData(start,end,now,dayWidth)
-
         let now=0;
         let dayWidth=30;
-        const wrapper = mount(<Headers 
-                                  
-                                    numVisibleDays={20}
+        const wrapper = shallow(<Headers 
+                                    numVisibleDays={30}
                                     currentday={1}
                                     nowposition={now}
                                     dayWidth={dayWidth}
                                     scrollLeft={0} />);
         expect(wrapper.find('#timeline-header')).toBeDefined();
         expect(wrapper.find('.timeLine-main-header-container')).toBeDefined();
-  
-
         let today=new Date()
         let dayofMonth=today.getDate()
         let month=today.getMonth()+1
