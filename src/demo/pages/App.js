652f542322c8d7721edcf40dfc079e3e1766612b
Ross Leitch <professional.balbatross@gmail.com>
2021-07-16 22:05:03 +1200

    Typescript initial migration

--- src/demo/pages/App.js
+++ src/demo/pages/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import TimeLine from 'libs/TimeLine';
+import TimeLine from '../../TimeLine';
 import Generator from './Generator';
 import './App.css';
 
--- src/demo/pages/App_Crud.js
+++ src/demo/pages/App_Crud.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import TimeLine from 'libs/TimeLine';
+import TimeLine from '../../TimeLine';
 import './App.css';
 
 class App extends Component {
