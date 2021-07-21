954b40172b8e8cc51f5bba2615f14eb56babb7ac
Ross Leitch <professional.balbatross@gmail.com>
2021-07-21 23:30:28 +1200

    demo

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -4,5 +4,5 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
-  base: '/react-timeline-gantt/'
+  base: './', //process.env.NODE_ENV == 'production' ? '/react-timeline-gantt/' : './'
 })
